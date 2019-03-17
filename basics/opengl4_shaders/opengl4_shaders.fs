0ffd0f14a5c777de2c6a1e590a0f818a1d91ea1b
Mikel Losada <mikel.losada@gmail.com>
2019-03-17 20:29:56 +0100

    Add colour uniform at opengl4_shaders to change colour dynamically

---
+++ basics/opengl4_shaders/opengl4_shaders.fs
@@ -0,0 +1,7 @@
+#version 410
+uniform vec4 inputColour;
+out vec4 frag_colour;
+
+void main(){
+  frag_colour = inputColour;//inputColour;
+}
---
+++ basics/opengl4_shaders/shader_read.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* get_shader(char* shader_file){
+  FILE *fp;
+  long lSize;
+  char *buffer;
+
+  fp = fopen (shader_file , "rb" );
+  if( !fp ) perror(shader_file),exit(1);
+
+  fseek( fp , 0L , SEEK_END);
+  lSize = ftell( fp );
+  rewind( fp );
+
+  /* allocate memory for entire content */
+  buffer = (char*)calloc( 1, lSize+1 );
+  if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
+
+  /* copy the file into the buffer */
+  if( 1!=fread( buffer , lSize, 1 , fp) )
+    fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
+
+  /* do your work here, buffer is a string contains the whole text */
+
+  fclose(fp);
+  
+  return buffer;
+}
+
+
+
+
