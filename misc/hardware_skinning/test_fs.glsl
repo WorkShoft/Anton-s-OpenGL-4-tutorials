7a45c315d53b8ca185f9d5fa4f2291d16df89513
Mikel Losada <mikel.losada@gmail.com>
2019-03-24 15:15:42 +0100

    Render Mario texture correctly with shaders

---
+++ misc/hardware_skinning/test_fs.glsl
@@ -0,0 +1,11 @@
+#version 410
+
+uniform sampler2D texture1;
+in vec3 normal;
+in vec2 st;
+out vec4 frag_colour;
+
+void main() {
+  vec4 texel_a = texture (texture1, st);
+  frag_colour = texel_a;
+}
